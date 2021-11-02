e4e7184c25129d168ebec473e892bf1171f94a76
Anderson Guerra <and.guerra@outlook.com>
2021-11-02 18:47:13 -0300

    avanços

--- src/components/FromFile.vue
+++ src/components/FromFile.vue
@@ -8,9 +8,25 @@
     </v-row>
     <v-row>
       <v-col>
+        <v-text-field v-model="process" label="Concurso"></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field v-model="date" label="Data"></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
         <v-file-input
           v-model="candidatesFile"
           placeholder="Clique aqui para inserir a planilha de candidatos"
+        ></v-file-input>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-file-input
+          v-model="ensalamentoFile"
+          placeholder="Clique aqui para inserir a planilha de ensalamento"
         ></v-file-input>
       </v-col>
     </v-row>
@@ -22,23 +38,39 @@
   </v-container>
 </template>
 <script>
-import generatePdf from "../services/generatePdf";
+import sortArray from "sort-array";
+import xlsx from "xlsx";
+// import generatePdf from "../services/generatePdf";
 export default {
   data: () => ({
+    process: "",
+    date: "",
     candidatesFile: null,
+    ensalamentoFile: null,
+    candidates: null,
+    ensalamento: null,
   }),
   methods: {
     async generatePdf() {
-      await generatePdf(
-        "ahoy",
-        {
-          name: "Anderson",
-          area: "hum",
-          number: "123",
-          place: "local",
-        },
-        1
-      );
+      const reader1 = new FileReader();
+      const reader2 = new FileReader();
+      reader1.onload = () => {
+        const workbook = xlsx.read(reader1.result);
+        const sheet = workbook.SheetNames[0];
+        let sheetJson = xlsx.utils.sheet_to_json(workbook.Sheets[sheet], {});
+        sheetJson = sortArray(sheetJson, {
+          by: ["AREA", "NOME DO CANDIDATO"],
+        });
+        this.candidates = sheetJson;
+      };
+      reader2.onload = () => {
+        const workbook = xlsx.read(reader2.result);
+        const sheet = workbook.SheetNames[0];
+        let sheetJson = xlsx.utils.sheet_to_json(workbook.Sheets[sheet], {});
+        this.ensalamento = sheetJson;
+      };
+      reader1.readAsArrayBuffer(this.candidatesFile);
+      reader2.readAsArrayBuffer(this.ensalamentoFile);
     },
   },
 };
