1b14faca35b1e21d9a95ac9c3eeb4dccc15fa4e3
Anderson <and.guerra@outlook.com>
2022-08-04 22:04:42 -0300

    meh

--- src/components/FromFile.vue
+++ src/components/FromFile.vue
@@ -14,31 +14,32 @@
         <v-text-field v-model="date" label="Data"></v-text-field>
       </v-col>
       <v-col>
-        <v-text-field
-          v-model="pageMaxNumber"
-          label="Número de páginas"
-        ></v-text-field>
+        <v-text-field v-model="pageMaxNumber" label="Número de páginas"></v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field v-model="idProcesso" label="Id do Processo Seletivo"></v-text-field>
       </v-col>
     </v-row>
     <v-row>
       <v-col>
-        <v-file-input
-          v-model="candidatesFile"
-          placeholder="Clique aqui para inserir a planilha de candidatos"
-        ></v-file-input>
+        <v-file-input v-model="candidatesFile" placeholder="Clique aqui para inserir a planilha de candidatos">
+        </v-file-input>
       </v-col>
     </v-row>
     <v-row>
       <v-col>
-        <v-file-input
-          v-model="ensalamentoFile"
-          placeholder="Clique aqui para inserir a planilha de ensalamento"
-        ></v-file-input>
+        <v-file-input v-model="ensalamentoFile" placeholder="Clique aqui para inserir a planilha de ensalamento">
+        </v-file-input>
       </v-col>
     </v-row>
     <v-row>
       <v-col>
         <v-btn color="primary" block @click="generatePdf">Gerar provas</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn color="primary" block @click="generatePdfNoSheet">Gerar provas (sem planilha)</v-btn>
       </v-col>
     </v-row>
     <loading :loading="loading" />
@@ -51,14 +52,16 @@ import generatePdfList from "../services/generatePdfList";
 import sortArray from "sort-array";
 import xlsx from "xlsx";
 import Loading from "./Loading.vue";
+import axios from "axios";
 export default {
   components: {
     Loading,
   },
   data: () => ({
     loading: false,
-    process: "",
-    date: "",
+    process: "Edital Nº 13/2022 - Professor Substituto - UNIFAP",
+    date: "07/08/2022",
+    idProcesso: '62c47ef9752588fd51b4d3e1',
     pageMaxNumber: 1,
     fileCounter: 0,
     candidatesFile: null,
@@ -126,6 +129,50 @@ export default {
     },
   },
   methods: {
+    async generatePdfNoSheet() {
+      let a = await axios.get(
+        `http://localhost:3030/inscriptions?id_ps=${this.idProcesso}&$populate=id_user`
+      );
+      let inscriptions = a.data.filter(e => e.room)
+      let salas = []
+      for (const inscription of inscriptions) {
+        if (!salas.includes(inscription.room))
+          salas.push(inscription.room)
+      }
+      const zip = new jszip();
+      for (const sala of salas) {
+        const inscriptionsInRoom = inscriptions.filter(e => e.room === sala)
+        const insFormated = inscriptionsInRoom.map(e => {
+          // area: candidatesInArea[0]["AREA"],
+          // number: candidatesInArea[0]["N. INSC."],
+          // name: candidatesInArea[0]["NOME DO CANDIDATO"],
+          // place: `${sala["BLOCOS"]} - SALA ${sala["SALA"]}`,
+          console.log(sala)
+          return {
+            area: e.course[0].title,//sala.split(" - ")[0],
+            number: e.inscriptionNumber,
+            name: e.id_user.name,
+            place: sala.split(" - ")[1]
+          }
+        })
+        const pdf = await generatePdfList(
+          insFormated,
+          this.process,
+          sala.split(" - ")[0],
+          sala,
+          this.date,
+          this.pageMaxNumber
+        );
+        zip.file(
+          `lista ${sala.replaceAll("/", "-")}.pdf`,
+          pdf,
+          { binary: true }
+        );
+      }
+      const zipFile = await zip.generateAsync({ type: "blob" });
+      saveAs(zipFile, "lista.zip");
+      console.log(salas, salas.length)
+    },
     async generatePdf() {
       this.loading = true;
       const reader1 = new FileReader();
